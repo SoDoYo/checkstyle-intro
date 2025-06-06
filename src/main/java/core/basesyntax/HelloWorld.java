e933adbdafc3753b5792e1e2840dd7d5f9b30f88
Danylo Kurtiak <zxxh127@gmail.com>
2025-06-06 02:56:59 +0200

    test

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,7 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Cat;
-import core.basesyntax.Model.Dog;
-
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
 public class HelloWorld {
@@ -11,32 +10,28 @@ public class HelloWorld {
     private int age;
     private int size;
 
-
     public static void main(String[] args) {
         Dog dog = new Dog();
         Cat cat = new Cat();
     }
-
     private String getFromOneToTen() {
         String result = "";
 
-        for (int i = 0; i < 10; i++){
-            if(i % 2 == 0) {
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
                 result = result + i + " ";
             }
         }
         return result;
     }
-
     private void sayHello(String hello) {
         System.out.println(
-                        "Hello, Mate! I am happy to see you! This class looks weird! " +
+                "Hello, Mate! I am happy to see you! This class looks weird! " +
                         "Your task is to fix the checkstyle in this file. Your goal is to run " +
                         "`mvn clean package` and receive `BUILD SUCCESS` message"
         );
         System.out.println(hello);
     }
-
     private void initializeVariables() {
         name = "Hello mates!";
         System.out.println(name);
